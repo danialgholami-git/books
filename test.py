a57ed1dafefffe073c8bccf5b4d0ada8ed3d2402
hamid <hamidgolaaminezhad@gmail.com>
2025-07-26 15:25:22 +0330

    just librarys

---
+++ test.py
@@ -0,0 +1,14 @@
+from typing import Union
+from fastapi import FastAPI
+import sqlite3
+
+app = FastAPI()
+conn = sqlite3.connect("data.sqlite")
+
+conn.execute("""CREATE TABLE IF NOT EXISTS books(
+   id INT PRIMARY KEY,
+   name TEXT,
+   author TEXT,
+   status PRESENT OR ABSENT);
+""")
+conn.commit()
